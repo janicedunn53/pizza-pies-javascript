4eb87c38f2a5dfad7599ee086ef7c67e6c6ff2b1
Janice Dunn <janicedunn53@gmail.com>
2015-09-18 12:33:23 -0700

    Add front-end.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Pizza(quantity, toppingCombo, pieSize) {
 Pizza.prototype.pieSizePrice = function() {
   var basePieSizePrice = 0;
 
-  if (this.pieSize === "Medium") {
+  if (this.pieSize === "medium") {
     return basePieSizePrice + 5;
   } else {
     return basePieSizePrice + 6;
@@ -17,9 +17,9 @@ Pizza.prototype.pieSizePrice = function() {
 Pizza.prototype.toppingComboPrice = function() {
   var baseToppingComboPrice = 0;
 
-  if (this.toppingCombo === "Italian Combo") {
+  if (this.toppingCombo === "italian-combo") {
     return baseToppingComboPrice + 5;
-  } else if (this.toppingCombo === "Veggie Combo") {
+  } else if (this.toppingCombo === "veggie-combo") {
     return baseToppingComboPrice + 6;
   } else {
     return baseToppingComboPrice + 7;
@@ -35,3 +35,19 @@ Pizza.prototype.quantityPrice = function() {
 Pizza.prototype.totalPrice = function() {
   return this.pieSizePrice() + this.toppingComboPrice() + this.quantityPrice();
 }
+
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+
+    var pieSizeInput = $('input[name=size]:checked').val();
+    var toppingComboInput = $('input[name=combo]:checked').val();
+    var quantityInput = parseInt($('input#quantity').val());
+
+    var newPizza = new Pizza(pieSizeInput, toppingComboInput, quantityInput);
+
+    $("#total-price").text("$" + newPizza.totalPrice() + ".00");
+
+    $(".show-price").show();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,28 +1,28 @@
 describe('Pizza', function() {
   it("creates a new pizza order with the given specifications", function() {
-    var testPizza = new Pizza(3, "Italian Combo", "Medium");
+    var testPizza = new Pizza(3, "italian-combo", "medium");
     expect(testPizza.quantity).to.equal(3);
-    expect(testPizza.toppingCombo).to.equal("Italian Combo");
-    expect(testPizza.pieSize).to.equal("Medium");
+    expect(testPizza.toppingCombo).to.equal("italian-combo");
+    expect(testPizza.pieSize).to.equal("medium");
   });
 
   it("adds the pieSizePrice method to all pizzas", function() {
-    var testPizza = new Pizza(3, "Italian Combo", "Medium");
+    var testPizza = new Pizza(3, "italian-combo", "medium");
     expect(testPizza.pieSizePrice()).to.equal(5);
   });
 
   it("adds the toppingComboPrice method to all pizzas", function() {
-    var testPizza = new Pizza(3, "Italian Combo", "Medium");
+    var testPizza = new Pizza(3, "italian-combo", "medium");
     expect(testPizza.toppingComboPrice()).to.equal(5);
   });
 
   it("adds the quantityPrice method to all pizza orders", function() {
-    var testPizza = new Pizza(3, "Italian Combo", "Medium");
+    var testPizza = new Pizza(3, "italian-combo", "medium");
     expect(testPizza.quantityPrice()).to.equal(9);
   });
 
   it("creates the total pizza price", function () {
-    var testPizza = new Pizza(3, "Italian Combo", "Medium");
+    var testPizza = new Pizza(3, "italian-combo", "medium");
     expect(testPizza.totalPrice()).to.equal(19);
   });
 });
