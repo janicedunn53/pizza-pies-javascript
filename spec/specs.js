054fec0523e69aea4112abd463c19b9cd2a1555a
Janice Dunn <janicedunn53@gmail.com>
2015-09-18 10:03:27 -0700

    Create a new pizza order with the given specifications.

--- spec/specs.js
+++ spec/specs.js
@@ -1,28 +1,8 @@
-describe('Ticket', function() {
-  it("creates a new Ticket with the given specifications", function() {
-    var testTicket = new Ticket("first-run", "matinee", 21);
-    expect(testTicket.movieRun).to.equal("first-run");
-    expect(testTicket.showTime).to.equal("matinee");
-    expect(testTicket.age).to.equal(21);
-  });
-
-  it("adds the movieRunPrice method to all tickets", function() {
-    var testTicket = new Ticket("first-run", "matinee", 21);
-    expect(testTicket.movieRunPrice()).to.equal(5);
-  });
-
-  it("adds the showTimePrice method to all tickets", function() {
-    var testTicket = new Ticket("first-run", "matinee", 21);
-    expect(testTicket.showTimePrice()).to.equal(4);
-  });
-
-  it("adds the agePrice method to all tickets", function() {
-    var testTicket = new Ticket("first-run", "matinee", 21);
-    expect(testTicket.agePrice()).to.equal(5);
-  });
-
-  it("create total movie price", function() {
-    var testTicket = new Ticket("first-run", "matinee", 21);
-    expect(testTicket.totalPrice()).to.equal(14);
+describe('Pizza', function() {
+  it("creates a new pizza order with the given specifications", function() {
+    var testPizza = new Pizza(3, "Queen", "Medium");
+    expect(testPizza.quantity).to.equal(3);
+    expect(testPizza.toppingCombo).to.equal("Queen");
+    expect(testPizza.pieSize).to.equal("Medium");
   });
 });
